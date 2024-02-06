ee84b1f45248d6d2423b4739a3d4c1b487ecaf65
Carey Duran <61742822+ByteWrencher@users.noreply.github.com>
2024-02-06 18:27:59 -0500

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -18,7 +18,7 @@ var config_data = `
     "gsCol": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2020ilch",
+      "defaultValue": "2023nyro",
       "required": "true"
     },
     { "name": "Match Level",
