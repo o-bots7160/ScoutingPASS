4812a742fe5c8e293ec01aa6e2375f62c82d3ac4
jrl2222 <jlcomputersolutions.jl@gmail.com>
2024-02-10 15:57:49 -0500

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -2,7 +2,7 @@ var config_data = `
 {
   "dataFormat": "tsv",
   "title": "Scouting PASS 2024",
-  "page_title": "Crescendo",
+  "page_title": "O-Bots Crescendo",
   "checkboxAs": "10",
   "prematch": [
     { "name": "Scouter Initials",
