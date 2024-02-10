de5e41fe6d778e888162612109e984b126c2f2f1
jrl2222 <jlcomputersolutions.jl@gmail.com>
2024-02-10 15:49:29 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "56UsemXpswsnH0J8aElT396wvvEXdLj4FWPnEeqp4h208B1yzGTmx9NenGIs6vJe";
 //var authKey = "NdOJSuD9ZaaJhyVJiKWVJZ1hmyKMTcERzKPbXTKqv5bBJ5O01s88GZXZHn0xxtwK";
 /**
  * Get list of teams in event
