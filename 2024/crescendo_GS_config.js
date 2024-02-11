533025cd2a87ff11c1a76e1fe010b572479b9ad3
jrl2222 <jlcomputersolutions.jl@gmail.com>
2024-02-10 20:06:43 -0500

    Update crescendo_GS_config.js

--- 2024/crescendo_GS_config.js
+++ 2024/crescendo_GS_config.js
@@ -1,7 +1,7 @@
 var config_data = `
 {
   "enable_google_sheets": "true",
-  "dataFormat": "tsv",
+  //"dataFormat": "tsv",
   "title": "Scouting PASS 2024",
   "page_title": "O-Bots 7160 Crescendo",
   "checkboxAs": "10",
